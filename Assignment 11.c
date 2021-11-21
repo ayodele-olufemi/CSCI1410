be20fa8ea23a940e868b996e855637cca7b43da1
Ayodele Olufemi <oae@localhost.localdomain>
2021-11-21 16:03:38 -0600

    move to new system

---
+++ Assignment 11.c
@@ -0,0 +1,60 @@
+//complete this program by coding the missing part. 
+
+#include <stdio.h>
+//declare the function prototypes here
+ 
+int main () {
+
+   // local variable definition 
+ 
+   // prompt the users to enter two integers 
+  
+  // call sum, subtract, quotient, multiply, max, min, and average functions 
+ 
+  // Print, display the result
+ 
+   return 0;
+}
+
+/* function returning the sum of the two numbers */
+int sum(num1, num2) {
+   return num1 + num 2;
+}
+
+/* function returning the difference of the two numbers */
+int difference(num1, num2) {
+   return num1 - num2;
+}
+
+/* function returning the quotient of the two numbers */
+int quotient(num1, num2) {
+   return num1 / num2;
+}
+
+/* function returning the quotient of the two numbers */
+int product(um1, num2) {
+   return num1 * num2;
+}
+
+/* function returning the min between two numbers */
+int min(num1, num2){
+   if (num1 < num2) {
+	return num1;
+	} else {
+	return num2;
+	}
+} 
+
+/* function returning the max between two numbers */
+int max(num1, num2) {
+   if (num1 > num2) {
+	return num1;
+	} else {
+	return num2;
+	}
+}
+
+/* function returning the average of the two numbers */
+int average(num1, num2){
+   return (num1 + num2) / 2;
+} 
